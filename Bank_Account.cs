84dc025313a86103c596db6db7befb83ed97a524
Moonyll <s.pouwel@laposte.net>
2019-01-31 09:36:51 +0100

    PO8

---
+++ Bank_Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace PO8_
+////{
+////        // class LaManu_Account
+////        //{
+////        //    public string _owner = "La Manu";
+////        //    public string _currency = "euros";
+////        //    public double _final_settlement = 2000;
+////        //    public double _credit = 19;
+////        //    public void _tocredit(double _credit)
+////        //    {
+////        //    _final_settlement = _final_settlement + _credit;
+////        //    Console.WriteLine("Le solde de " + _owner + " est de " + _final_settlement + _currency);
+////        //   }
+                    
+////        //} 
+//// }
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO8_
+{
+     class Program
+    {
+     static void Main(string[] args)
+        {
+            Bank_Account CompteLaManu = new Bank_Account(); // Déclaration & instanciation de l'objet CompteLaManu avec la classe Bank_Account
+            Console.WriteLine("Titulaire du compte ?"); // Nom titulaire
+            CompteLaManu.owner = Console.ReadLine(); // L'attribut owner de l'objet CompteLaManu reçoit la valeur entrée par l'utilisateur 
+            Console.WriteLine("Solde Tout compte ?"); // Solde bancaire
+            CompteLaManu.final_settlement = double.Parse(Console.ReadLine());
+            Console.WriteLine("Devise?"); // Devise
+            CompteLaManu.currency = Console.ReadLine();
+            Console.WriteLine("Montant du crédit ?"); // Montant du crédit
+            double amount = double.Parse(Console.ReadLine());
+            /*obj.owner = "La Manu";*/
+            /* obj.currency = "Euros";*/
+            /*obj.final_settlement = 2000;*/
+            /*obj.credit = 19;*/
+            //CompteLaManu.debit = 0;
+            CompteLaManu.Tocredit(amount); // Appel de la méthode Tocredit
+            //obj.Todebit(obj.debit);
+        }
+    }
+    //déclaration de la classe la manu 
+    public class Bank_Account
+    {
+        public string owner;
+        public string currency;
+        public double final_settlement;
+        public void Tocredit(double amount) //déclaration de la méthode Tocredit type void qui ne retourne rien
+            {
+            final_settlement = final_settlement + amount;
+            Console.WriteLine("Le solde de " + owner + " est de " + final_settlement +" "+ currency);
+            }
+        public void Todebit(double amount)
+            {
+            final_settlement = final_settlement - amount;
+            Console.WriteLine("Le solde de " + owner + " est de " + final_settlement +" "+ currency);
+            }
+    }
+}
